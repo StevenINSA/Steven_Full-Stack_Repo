3749cfeed03ad67543fa9d1f6a6369c1d7bf078e
StevenINSA <>
2022-05-10 22:38:00 +0300

    3rd video: creating new users with hashed password

---
+++ meanautapp/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+//Get the user by id
+module.exports.getUserById = function(id, callback){
+    User.findById(id, callback);
+};
+
+//Get the user by username
+module.exports.getUserById = function(id, callback){
+    const query = {username: username};
+    User.findOne(query, callback);
+};
+
+//Add user function (see /routes/users.js Register section)
+module.exports.addUser = function(newUser, callback){
+    bcrypt.genSalt(10, (err, salt) => {     //will hash the password. Gensalt is like a random key that will hash the password
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;    //replace password by the hashed one
+            newUser.save(callback);     //and save it
+        });
+    });    
+};
--- meanautapp/routes/users.js
+++ meanautapp/routes/users.js
@@ -1,26 +1,42 @@
 // All of users' routes
 
 const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const { addListener } = require('../models/user');
+
 const router = express.Router();
 
+
 //Register route
-router.get('/register', (req, res, next) => {   //as we are in the folder users.js, the route here is localhost:3000/users/register
-    res.send('REGISTER');
+router.post('/register', (req, res, next) => {   //as we are in the folder users.js, the route here is localhost:3000/users/register
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    // Call the function Add user from /models/user (store in the database?)
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({sucess: false, msg: 'Failed to register'});
+        } else {
+            res.json({success: true, msg:'User registered'});
+        }
+    });
 });
 
 //Authenticate route
-router.get('/authenticate', (req, res, next) => {  
+router.post('/authenticate', (req, res, next) => {  
     res.send('AUTHENTICATE');
 });
 
 //Profile route
 router.get('/profile', (req, res, next) => {  
     res.send('PROFILE');
-});
-
-//Validate route
-router.get('/validate', (req, res, next) => {  
-    res.send('VALIDATE');
 });
 
 module.exports = router;
