2c46e831728e49e7f59e93b3f39013741fe688af
StevenINSA <>
2022-05-13 07:06:02 +0300

    Field checking during registration

--- meanautapp/angular-src/src/app/app.module.ts
+++ meanautapp/angular-src/src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+import { ValidateService } from './services/validate.service';
+
+const appRoutes: Routes = [   //All our routes
+  { path: '', component: HomeComponent},  //Home page
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+]
 
 @NgModule({
-  declarations: [
+  //import components
+  declarations: [   
     AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
-  imports: [    //Imports modules
-    BrowserModule
+  //Imports modules
+  imports: [    
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],  //Imports components
+  //Imports services
+  providers: [  
+    ValidateService
+  ], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
