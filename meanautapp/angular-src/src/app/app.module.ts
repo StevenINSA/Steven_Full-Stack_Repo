47d6bfcb763539ab950bfc1a55c0993460952214
StevenINSA <>
2022-05-15 12:06:31 +0300

    Register with angular, done

--- meanautapp/angular-src/src/app/app.module.ts
+++ meanautapp/angular-src/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from "@angular/router";
 import { FormsModule } from '@angular/forms';
 import { FlashMessagesModule } from 'angular2-flash-messages';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -13,6 +14,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
 
 const appRoutes: Routes = [   //All our routes
   { path: '', component: HomeComponent},  //Home page
@@ -38,11 +40,13 @@ const appRoutes: Routes = [   //All our routes
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     FormsModule,
-    FlashMessagesModule.forRoot()
+    FlashMessagesModule.forRoot(),
+    HttpClientModule
   ],
   //Imports services
   providers: [  
-    ValidateService
+    ValidateService,
+    AuthService
   ], 
   bootstrap: [AppComponent]
 })
--- meanautapp/angular-src/src/app/components/register/register.component.ts
+++ meanautapp/angular-src/src/app/components/register/register.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {ValidateService} from '../../services/validate.service'
+import { ValidateService } from '../../services/validate.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({    
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
   name: String;
   username: String;
   email: String;
   password: String;
+  dataRegister: any;
 
-  constructor(private validateService: ValidateService, private _flashMessagesService:FlashMessagesService) { }
+  constructor(private validateService: ValidateService, 
+    private _flashMessagesService:FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
   }
@@ -37,6 +46,20 @@ export class RegisterComponent implements OnInit {
       this._flashMessagesService.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 6000});
       return false;
     }
-  }
 
+    // Register user
+    this.authService.registerUser(user).subscribe(data => {
+      console.log(data);
+      this.dataRegister = data;
+
+      if(this.dataRegister.success){
+        this._flashMessagesService.show('You are now registered', {cssClass: 'alert-success', timeout: 6000});
+        // Redirect to login page after registered
+        this.router.navigate(['/login']);
+      } else {
+        this._flashMessagesService.show('Registration went bad, please try again', {cssClass: 'alert-danger', timeout: 6000});
+        this.router.navigate(['/register']);
+      }
+    });
+  }
 }
